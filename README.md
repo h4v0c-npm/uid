e5de73669cca18a009e9cb07240340177b27f479
John Wakley <john.wakley@icloud.com>
2023-04-30 20:03:51 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# uid
+# @h4v0c/uid
+A unique id generator. This does NOT confirm to any standards.
+
+## Install:
+`npm install @h4v0c/uid`
+
+## Usage:
+    ```javascript
+    import { uid } from "@h4v0c/uid";
+
+    const id = uid();
+    ```
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { RandomNumberGenerator } from '@h4v0c/rng';
+
+let _rng: RandomNumberGenerator | undefined = undefined;
+let _src_string: string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+export type UID = string;
+
+export const uid = (length: number = 16): UID => {
+    if (_rng === undefined) {
+        _rng = new RandomNumberGenerator();
+        _src_string = _rng.shuffle(_src_string) as string;
+    }
+
+    const uuid: Array<UID> = [];
+
+    while (uuid.length < length) uuid.push(_rng.choose(_src_string));
+
+    return uuid.join('');
+}
