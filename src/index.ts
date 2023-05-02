b415cbcc64f66cedbde391f21ac9fc08ab525595
John Wakley <john.wakley@icloud.com>
2023-05-02 07:14:13 -0400

    removed the UID type, replaced with string

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,13 @@ import { RandomNumberGenerator } from '@h4v0c/rng';
 let _rng: RandomNumberGenerator | undefined = undefined;
 let _src_string: string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-export type UID = string;
-
-export const uid = (length: number = 16): UID => {
+export const uid = (length: number = 16): string => {
     if (_rng === undefined) {
         _rng = new RandomNumberGenerator();
         _src_string = _rng.shuffle(_src_string) as string;
     }
 
-    const uuid: Array<UID> = [];
+    const uuid: Array<string> = [];
 
     while (uuid.length < length) uuid.push(_rng.choose(_src_string));
 
