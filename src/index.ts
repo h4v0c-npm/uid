6a7ac8bce8172a7f7af70aa4ec65e8dbb71b884b
John Wakley <john.wakley@icloud.com>
2023-05-28 09:34:45 -0400

    added type: UID (string)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { RandomNumberGenerator } from '@h4v0c/rng';
 let _rng: RandomNumberGenerator | undefined = undefined;
 let _src_string: string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-export const uid = (length: number = 16): string => {
+export declare type UID = string;
+
+export const uid = (length: number = 16): UID => {
     if (_rng === undefined) {
         _rng = new RandomNumberGenerator();
         _src_string = _rng.shuffle(_src_string) as string;
